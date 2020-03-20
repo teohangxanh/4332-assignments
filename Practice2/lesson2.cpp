3017913194a050cd629556e76e10039f42897b45
teohangxanh <47458301+teohangxanh@users.noreply.github.com>
2020-03-19 19:26:00 -0600

    ass3: Created a triangle in the middle of the screen

---
+++ Practice2/lesson2.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <sstream>
+//#define GLEW_STATIC
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+using namespace std;
+
+const char* TITLE = "Introduction - Triangle";
+const int gWindowWidth = 800;
+const int gWindowHeight = 600;
+bool initOpenGL();
+GLFWwindow* gWindow = NULL;
+
+const GLchar* vertexShaderSrc = " ";
+const GLchar* fragmentShaderSrc = " ";
+
+void glfw_onkey(GLFWwindow* window, int key, int scancode, int action, int mode);
+void showFPS(GLFWwindow* window);
+
+int main() {
+	if (!initOpenGL()) {
+		cerr << "GLFW Initialization failed!" << endl;
+		return false;
+	}
+
+	GLfloat vertices[] = {
+		0.0f, 0.5f, 0.0f,		// Top
+		-0.5f, -0.5f, 0.0f,		// Left
+		0.5, -0.5f, 0.0f		// Right
+	};
+
+	GLuint vao, vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(0);
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertexShaderSrc, NULL);
+	glCompileShader(vs);
+
+	GLint result;
+	GLchar infolog[512];
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &result);
+	if (!result) {
+		glGetShaderInfoLog(vs, sizeof(infolog), NULL, infolog);
+		cout << "Error! Vertex shader failed to compile. " << infolog << endl;
+	}
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragmentShaderSrc, NULL);
+	glCompileShader(fs);
+	glGetShaderiv(fs, GL_COMPILE_STATUS, &result);
+	if (!result) {
+		glGetShaderInfoLog(fs, sizeof(infolog), NULL, infolog);
+		cout << "Error! Fragment shader failed to compile. " << infolog << endl;
+	}
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vs);
+	glAttachShader(shaderProgram, fs);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &result);
+	if (!result) {
+		glGetProgramInfoLog(shaderProgram, sizeof(infolog), NULL, infolog);
+		cout << "Error! Shader program linker failure." << infolog << endl;
+	}
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	// Main loop
+	while (!glfwWindowShouldClose(gWindow)) {
+		showFPS(gWindow);
+		glfwPollEvents();
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
+		glfwSwapBuffers(gWindow);
+	}
+
+	glDeleteProgram(shaderProgram);
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
+
+	glfwTerminate();
+
+	return 0;
+}
+
+void glfw_onkey(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
+
+bool initOpenGL() {
+	if (!glfwInit()) {
+		cerr << "GLFW initialization failed!" << endl;
+		return false;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	gWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, TITLE, NULL, NULL);
+
+	if (gWindow == NULL) {
+		cerr << "Failed to create GLFW Window" << endl;
+		glfwTerminate();
+		return false;
+	}
+
+	glfwMakeContextCurrent(gWindow);
+
+	glfwSetKeyCallback(gWindow, glfw_onkey);
+
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK) {
+		cerr << "Glew initialization failed!" << endl;
+		return false;
+	}
+
+	glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+	return true;
+}
+
+void showFPS(GLFWwindow* window) {
+	static double previousSeconds = 0.0;
+	static int frameCount = 0;
+	double elapsedSeconds;
+	double currentSeconds = glfwGetTime(); // returns number of seconds sicen GLFW started, as a double
+	elapsedSeconds = currentSeconds - previousSeconds;
+
+	// limit text update 4 times per second
+	if (elapsedSeconds > 0.25) {
+		previousSeconds = currentSeconds;
+		double fps = (double)frameCount / elapsedSeconds;
+		double msPerFrame = 1000.0 / fps;
+		ostringstream outs;
+		outs.precision(3);
+		outs << fixed
+			<< TITLE << "     "
+			<< "FPS: " << fps << "     "
+			<< "Frame time: " << msPerFrame << " (ms)";
+		glfwSetWindowTitle(window, outs.str().c_str());
+
+		frameCount = 0;
+	}
+	frameCount++;
+}
